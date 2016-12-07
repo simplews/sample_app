46241c0de84bfe1bed3bf23ac65a3f2b562438ee
simplews <skipenko@simple.if.ua>
2016-12-08 01:21:10 +0200

    Implement basic login (version with some errors)

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,28 +5,28 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_no_difference 'User.count' do
       post signup_path, params: { user: { name:  "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
+                                          email: "user@invalid",
+                                          password:              "foo",
+                                          password_confirmation: "bar" } }
     end
 
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.alert-danger.alert'
     assert_select 'form[action="/signup"]'
-end
+  end
 
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post signup_path, params: { user: { name:  "Example User",
-                                          email: "mail@example.org",
+      post signup_path, params: { user: { name:  "Andriy",
+                                          email: "skipenko@gmail.com",
                                           password:              "qwerty",
                                           password_confirmation: "qwerty" } }
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.empty?
+    assert is_logged_in?
   end
 
 end
