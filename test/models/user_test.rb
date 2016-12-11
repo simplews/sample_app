840f85ac4c55b745fefeced0126c2875554080bc
simplews <skipenko@simple.if.ua>
2016-12-12 01:26:43 +0200

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,12 +11,12 @@ class UserTest < ActiveSupport::TestCase
     assert @user.valid?
   end
 
-    test "name should be present" do
+  test "name should be present" do
     @user.name = "     "
     assert_not @user.valid?
   end
 
-    test "email should be present" do
+  test "email should be present" do
     @user.email = "     "
     assert_not @user.valid?
   end
@@ -30,7 +30,7 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
-test "email validation should reject invalid addresses" do
+  test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
@@ -46,14 +46,14 @@ test "email validation should reject invalid addresses" do
     assert_not duplicate_user.valid?
   end
 
-    test "email addresses should be saved as lower-case" do
+  test "email addresses should be saved as lower-case" do
     mixed_case_email = "Foo@ExAMPle.CoM"
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
-    test "password should be present (nonblank)" do
+  test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
@@ -67,11 +67,40 @@ test "email validation should reject invalid addresses" do
     assert_not @user.authenticated?(:remember, '')
   end
 
-    test "associated microposts should be destroyed" do
+  test "associated microposts should be destroyed" do
     @user.save
     @user.microposts.create!(content: "Lorem ipsum")
     assert_difference 'Micropost.count', -1 do
       @user.destroy
+    end
+  end
+
+ test "should follow and unfollow a user" do
+    michael  = users(:michael)
+    archer   = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
+
+    test "feed should have the right posts" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # Posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
     end
   end
 end
