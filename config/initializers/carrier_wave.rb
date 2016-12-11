630e5d1b11e08f9f9ca1b9c4b7db1fb50fc67b62
simplews <skipenko@simple.if.ua>
2016-12-11 23:08:15 +0200

    Add user microposts

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
       :aws_access_key_id     => ENV['AKIAJNVBY5E36TP5GY6Q'],
       :aws_secret_access_key => ENV['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'],
     }
-    :region => ENV['eu-central-1']
+    :region => ENV['eu-central-1'],
     config.fog_directory     =  ENV['herokutestforme']
   end
 end
