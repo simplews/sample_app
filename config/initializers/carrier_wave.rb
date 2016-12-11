ab05c061185bdd67b3f489acf191530ce37d57e8
simplews <skipenko@simple.if.ua>
2016-12-11 23:08:01 +0200

    Add user microposts

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,11 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      # Configuration for Amazon S3
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['AKIAJNVBY5E36TP5GY6Q'],
       :aws_secret_access_key => ENV['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'],
-      :region => ENV['eu-central-1']
     }
+    :region => ENV['eu-central-1']
     config.fog_directory     =  ENV['herokutestforme']
   end
 end
