df4a1974da2cfeaf77582351339af3712072f6c7
simplews <skipenko@simple.if.ua>
2016-12-11 23:04:20 +0200

    Add user microposts

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,6 +5,7 @@ if Rails.env.production?
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['AKIAJNVBY5E36TP5GY6Q'],
       :aws_secret_access_key => ENV['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']
+      :region => ENV['eu-central-1']
     }
     config.fog_directory     =  ENV['herokutestforme']
   end
